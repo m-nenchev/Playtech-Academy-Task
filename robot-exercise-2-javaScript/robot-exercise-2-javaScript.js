5dfabe818be83c9cca7064950c91fdd47e892b64
m-nenchev <milannn28@abv.bg>
2022-04-10 14:03:43 +0300

    Add files via upload

---
+++ robot-exercise-2-javaScript/robot-exercise-2-javaScript.js
@@ -0,0 +1,12 @@
+const robot1 = [
+    {name:"Gosho"},
+    {color:""},
+    {type:"male",type1:"female"},
+    {receivedPhrases:[`"Hello"${robot2.name}`]}
+]
+const robot2 = [
+    {name:"Pesho"},
+    {color:""},
+    {type:"male",type1:"female"},
+    {receivedPhrases:[`"Hello" ${robot1.name}`]}
+]
